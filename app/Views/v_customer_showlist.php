ff018a2536ef219eeaed801fa5c0947cca7d9995
Pezl05 <72877459+Pezl05@users.noreply.github.com>
2021-07-30 01:28:02 +0700

    เพิ่มเติม v_customer_showlist

--- app/Views/v_customer_showlist.php
+++ app/Views/v_customer_showlist.php
@@ -22,8 +22,14 @@
     box-shadow: none;
 }
 
-@media (min-width: 768px) {
+@media (min-width: 1000px){
     .md\:grid-cols-2 {
+        grid-template-columns: repeat(3,minmax(0,1fr));
+    }
+}
+
+@media (min-width: 1450px) {
+    .xl\:grid-cols-3 {
         grid-template-columns: repeat(6, minmax(0, 1fr));
     }
 }
@@ -51,13 +57,13 @@
         <input class="pl-2 shadow-sm rounded" type="text" name="daterange" value="01/01/2018 - 01/15/2018" style=" height: 40px; width: 200px; ">
     </div>
 
-    <!-- Card -->
-    <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-2">
+    <!-- Card ตู้เข้า -->
+    <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-3">
         <!-- Card ตู้เข้า -->
         <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 ">
-            <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+            <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="1 1 17 10">
+                    <path d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z">
                     </path>
                 </svg>
             </div>
@@ -75,9 +81,9 @@
         </div>
         <!-- Card ตู้ออก-->
         <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-            <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+            <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-orange-500">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="-1 1 17 10">
+                    <path fill-rule="evenodd" d="M14.854 4.854a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 4H3.5A2.5 2.5 0 0 0 1 6.5v8a.5.5 0 0 0 1 0v-8A1.5 1.5 0 0 1 3.5 5h9.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4z">
                     </path>
                 </svg>
             </div>
@@ -96,8 +102,8 @@
         <!-- Card ตู้ดรอป -->
         <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
             <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+                <svg class="w-5 h-5" fill="currentColor" viewBox="3 2 10 11">
+                    <path d="M6 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5zm4 0a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z">
                     </path>
                 </svg>
             </div>
@@ -114,13 +120,9 @@
             </div>
         </div>
         <!-- Card Dry Container -->
-        <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-            <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                </svg>
-            </div>
-            <div>
+        <div class="flex items-center pl-3 py-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <img src="<?= base_url('/upload').'/'.'DryContainer.jpg'?>" alt="" style="width: 3rem; height: 3rem; object-fit: cover;">
+            <div class="pl-3">
                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
                     Dry Container
                 </p>
@@ -133,14 +135,9 @@
             </div>
         </div>
         <!-- Card Reefer container -->
-        <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-            <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z">
-                    </path>
-                </svg>
-            </div>
-            <div>
+        <div class="flex items-center pl-3 py-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <img src="<?= base_url('/upload').'/'.'ReeferContainer.jpg'?>" alt="" style="width: 4rem; height: 3rem; object-fit: cover;">
+            <div class="pl-4">
                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
                     Reefer Container
                 </p>
@@ -153,13 +150,9 @@
             </div>
         </div>
         <!-- Card ISO Tank Container -->
-        <div class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-            <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd"></path>
-                </svg>
-            </div>
-            <div>
+        <div class="flex items-center pl-3 py-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <img src="<?= base_url('/upload').'/'.'ISOTank.jpg'?>" alt="" style="width: 4rem; height: 3rem; object-fit: cover;">
+            <div class="pl-4">
                 <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
                     ISO Tank Container
                 </p>
@@ -242,7 +235,7 @@
 
 </div>
 
-<!-- Modal -->
+<!-- Modal ยืนยันการลบ -->
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -254,19 +247,18 @@
             </div>
             <form action="<?php echo base_url() . '/public/Customer_show/customer_delete'?>" method="post">
                 <div class="modal-body float-center">
-                    <input name="cus_id" id="cus_id" type="hidden">
+                    <!-- เก็บ Customer Id -->
+                    <input name="cus_id" id="cus_id" type="text">
                     คุณเเน่ใจหรือไม่ที่ต้องการลบ
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">ยกเลิก</button>
-                    <input type="submit" class="btn btn-danger" value="ลบ" data-dismiss="modal">
+                    <input type="submit" class="btn btn-danger" value="ลบ">
                 </div>
             </form>
         </div>
     </div>
 </div>
-<!-- ตัวลิ่๊ง -->
-<?php //echo base_url() . '/public/Customer_show/customer_delete/' . $arr_customer[$i]->cus_id ?>
 
 <script>
 $(document).ready(function() {
