e81b3b49f6b06b885e19cd3827aa940b4cfe3d61
62160150 <62160150@go.buu.ac.th>
2021-07-30 16:28:34 +0700

    รายชื่อ agent

--- app/Models/M_cdms_agent.php
+++ app/Models/M_cdms_agent.php
@@ -6,7 +6,7 @@ class M_cdms_agent extends Da_cdms_agent {
     // M get_all get_by_id
     public function get_all()
     {
-        $sql = "SELECT agn_id , con_agn_id , agn_company_name , agn_firstname , agn_lastname , agn_tel ,agn_tax FROM $this->table LEFT JOIN cdms_container ON con_agn_id = agn_id";
+        $sql = "SELECT * FROM $this->table";
         return $this->db->query($sql)->getResult();
     }   
     //con_agn_id , agn_company_name , agn_firstname , agn_lastname ,'agn_tel'   INNER JOIN cdms_container ON con_agn_id = agn_id"
