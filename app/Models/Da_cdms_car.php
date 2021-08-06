c64ba5351b4df47a56884d4025540efb68c9dae1
Nice2Meet-U <72909567+Nice2Meet-U@users.noreply.github.com>
2021-08-06 21:48:02 +0700

    update พัง //คอมเมนต์ไว้ก่อน

--- app/Models/Da_cdms_car.php
+++ app/Models/Da_cdms_car.php
@@ -39,17 +39,17 @@ class Da_cdms_car extends Model {
     * @author Nattanan
     * @Create Date 2564-08-06
     * @Update Date
-    */
-    public function update($car_id= NULL, bool $purge = false){
-        $sql = "UPDATE $this->table
-                SET car_code = ?, car_number = ?, car_chassis_number = ?, car_brand = ?,
-                        car_register_year = ?, car_weight = ?, car_branch = ?, car_fuel_type = ?, car_image = ?, car_status = ?, car_prov_id = ?, car_cart_id = ?
-                WHERE car_id = '$car_id' "; // ? = ค่าที่เราจะใส่ไปอยู่แล้ว , อย่าใช้ " ' " เพราะอาจจะเออเร่อได้
-        $this->db->query($sql, array($this->car_code ,$this->car_number ,$this->car_chassis_number ,$this->car_brand ,$this->car_register_year 
-                                                                ,$this->car_weight ,$this->car_branch ,$this->car_fuel_type ,$this->car_image ,$this->car_status 
-                                                                ,$this->car_prov_id ,$this->car_cart_id); //ถ้า SQL ที่เราใส่มี ? ต้องใส่ array ด้วย
+    // */
+    // public function update($car_id= NULL, bool $data = NULL){
+    //     $sql = "UPDATE $this->table
+    //             SET car_code = ?, car_number = ?, car_chassis_number = ?, car_brand = ?,
+    //                     car_register_year = ?, car_weight = ?, car_branch = ?, car_fuel_type = ?, car_image = ?, car_status = ?, car_prov_id = ?, car_cart_id = ?
+    //             WHERE car_id = '$car_id' "; // ? = ค่าที่เราจะใส่ไปอยู่แล้ว , อย่าใช้ " ' " เพราะอาจจะเออเร่อได้
+    //     $this->db->query($sql, array($this->car_code ,$this->car_number ,$this->car_chassis_number ,$this->car_brand ,$this->car_register_year 
+    //                                                             ,$this->car_weight ,$this->car_branch ,$this->car_fuel_type ,$this->car_image ,$this->car_status 
+    //                                                             ,$this->car_prov_id ,$this->car_cart_id)); //ถ้า SQL ที่เราใส่มี ? ต้องใส่ array ด้วย
 
-    }
+    // }
 
     
 }
