c6b1a534c5583e0fc78401588c9522a7d7fdf318
Gracewrs <62160108@go.buu.ac.th>
2021-08-07 14:14:05 +0700

    Driver_edit (ยังไม่เสร็จ)

---
+++ app/Controllers/Driver_edit.php
@@ -0,0 +1,26 @@
+<?php namespace App\Controllers;
+use App\Models\M_cdms_driver;
+
+/*
+    * Driver_update
+    * แสดงหน้าจอเเก้ไขข้อมูลพนักงาน 
+    * @author  Warisara
+    * @Create Date 2564-08-07
+    * @Update Date 2564-08-07
+*/
+class driver_edit extends Cdms_controller
+{
+/*   
+    * driver_update
+    * แสดงหน้าจอ driver_update
+    * @author  Warisara
+    * @Create Date 2564-08-07
+    * @Update Date 2564-08-07
+*/
+    public function driver_edit($dri_car_id)
+    {   
+        
+        
+    }
+
+}
--- app/Models/Da_cdms_driver.php
+++ app/Models/Da_cdms_driver.php
@@ -38,4 +38,18 @@ class Da_cdms_driver extends Model{
             VALUES ('$dri_name','$dri_tel','$dri_card_number','$dri_license','$dri_license_type','$dri_profile_image','$dri_status','$dri_date_start','$dri_date_end','$dri_car_id')";
         $this->db->query($sql);
     }
+
+    /*
+    * update
+    * เเก้ไขข้อมูลพนักงานขับรถ
+    * @author  Warisara
+    * @Create Date 2564-08-07
+    * @Update Date 2564-08-07
+*/   
+public function driver_update($dri_id=NULL,$dri_tel= NULL, $dri_card_number= NULL, $dri_license= NULL, $dri_license_type= NULL, $dri_profile_image= NULL, $dri_status= NULL, $dri_date_start= NULL, $dri_date_end= NULL, $dri_car_id= NULL){
+    $sql = "UPDATE $this->table SET dri_name='$dri_name',dri_tel='$dri_tel',dri_card_number='$dri_card_number',
+    dri_license='$dri_license', dri_license_type='$dri_license_type',dri_profile_image='$dri_profile_image',dri_status='$dri_status',dri_date_start='$dri_date_start',dri_date_end='$dri_date_end'
+    WHERE dri_car_id='$dri_car_id' ";
+    $this->db->query($sql);
+}
 }
