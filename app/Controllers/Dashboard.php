7520d1b388f8e01d597166396d2ad721f631f69a
Wirat Sakorn <67257309+wirat0155@users.noreply.github.com>
2021-08-12 12:46:35 +0700

    สี dashboard

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -3,6 +3,8 @@ use App\Models\M_cdms_car_type;
 class Dashboard extends Cdms_controller
 {
     public function dashboard_show() {
+        $_SESSION['menu'] = 'Dashboard';
+
         // get all car type
         $m_cart = new M_cdms_car_type();
         $data['arr_car_type'] = $m_cart->get_all();
