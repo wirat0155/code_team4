851692d3e08b7449928aa9b826c436e08843b22e
Wirat Sakorn <67257309+wirat0155@users.noreply.github.com>
2021-09-17 14:15:41 +0700

    ปรับขนาด schollbar

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -99,18 +99,19 @@
       /* width */
       ::-webkit-scrollbar {
         width: 5px;
+        height: 5px;
       }
 
       /* Track */
       ::-webkit-scrollbar-track {
-        box-shadow: inset 0 0 5px grey; 
+        box-shadow: inset 0 0 5px #f0f0f0; 
         border-radius: 10px;
       }
       
       /* Handle */
       ::-webkit-scrollbar-thumb {
-        background: #aaa; 
-        border-radius: 10px;
+        background: #dedede;
+        border-radius: 6px;
       }
       li {
         padding: 0px;
