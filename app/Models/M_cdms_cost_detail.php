0abd0bbf61f7245e5e5756d6cef7e4b54829d661
Wirat Sakorn <67257309+wirat0155@users.noreply.github.com>
2021-09-17 17:36:26 +0700

    เขียนคอมเม้น

--- app/Models/M_cdms_cost_detail.php
+++ app/Models/M_cdms_cost_detail.php
@@ -6,38 +6,55 @@ use App\Models\Da_cdms_cost_detail;
 
 /*
 * M_cdms_cost_detail
-* ดึงข้อมูลตู้คอนเทนเนอร์
-* @author Wirat
-* @Create Date 2564-07-29
-* @Update Date 2564-08-07
+* ดึงข้อมูลค่าใช้จ่าย
+* @author Nattdanai
+* @Create Date 2564-09-16
+* @Update Date 2564-09-17
 */
-
 class M_cdms_cost_detail extends Da_cdms_cost_detail
 {
 
     /*
-    * get_all
-    * ดึงข้อมูลตู้คอนเทนเนอร์ทั้งหมด
-    * @input - 
-    * @output array of container
-    * @author Wirat
-    * @Create Date 2564-07-29
-    * @Update Date
+    * get_by_ser_id
+    * ดึงข้อมูลค่าใช้จ่ายของบริการ
+    * @input ser_id
+    * @output array of cosd
+    * @author Nattdanai
+    * @Create Date 2564-09-16
+    * @Update Date 2564-09-16
     */
-    public function get_by_ser_id($ser_id)
-    {
+    public function get_by_ser_id($ser_id){
         $sql = "SELECT * FROM $this->table  
                 WHERE cosd_ser_id='$ser_id' and cosd_status=1
                 ORDER BY cosd_id DESC";
         return $this->db->query($sql)->getResult();
     }
-    public function get_by_id($cosd_id)
-    {
+
+    /*
+    * get_by_id
+    * ดึงข้อมูลค่าใช้จ่ายตาม id
+    * @input cosd_id
+    * @output array of cosd
+    * @author Nattdanai
+    * @Create Date 2564-09-16
+    * @Update Date 2564-09-16
+    */
+    public function get_by_id($cosd_id) {
         $sql = "SELECT * FROM $this->table  
                 WHERE cosd_id='$cosd_id' and cosd_status=1
                 ORDER BY cosd_id DESC";
         return $this->db->query($sql)->getResult();
     }
+
+    /*
+    * get_last
+    *ดึงข้อมูลค่าใช้จ่าย รายการล่าสุด
+    * @input -
+    * @output array of cosd
+    * @author Nattdanai
+    * @Create Date 2564-09-17
+    * @Update Date 2564-09-17
+    */
     public function get_last() {
         $sql = "SELECT * FROM $this->table ORDER BY cosd_id DESC LIMIT 1";
         return $this->db->query($sql)->getResult();
