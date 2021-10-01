9c462065ee1d1067d510b4bfba096cb32fa905fc
Thanatip Boonnat <62160012@go.buu.ac.th>
2021-10-01 22:18:34 +0700

    update showlist

--- app/Views/v_driver_showlist.php
+++ app/Views/v_driver_showlist.php
@@ -1,21 +1,21 @@
 <div class="ui modal">
     <i class="close icon"></i>
     <div class="header">
-        Remove service ?
+        Remove Driver ?
     </div>
     <div class="content">
         <form action="<?php echo base_url() . '/Driver_show/driver_delete' ?>" method="post">
             <input type="hidden" id="dri_id" name="dri_id">
 
-            <p style="font-size: 1rem">Are you sure to remove the service</p>
+            <p style="font-size: 1rem">Are you sure to remove the Customer</p>
 
             <div class="ui info message">
                 <div class="header">
-                    What happening after remove the service
+                    What happening after remove the Customer
                 </div>
                 <ul class="list">
-                    <li>The service still ramain in database,</li>
-                    <li>But you cannot see the service anymore</li>
+                    <li>The customer still ramain in database,</li>
+                    <li>But you cannot see the customer anymore</li>
                 </ul>
             </div>
     </div>
@@ -33,39 +33,36 @@
 <div class="main-panel">
     <div class="content">
         <div class="page-inner">
-            <div class="page-header">
-                <h4 class="page-title">Driver list</h4>
-                <ul class="breadcrumbs">
-                    <li class="nav-home">
-                        <a href="<?php echo base_url() . '/Dashboard/dashboard_show' ?>">
-                            <i class="flaticon-home"></i>
-                        </a>
-                    </li>
-                    <li class="separator">
-                        <i class="flaticon-right-arrow"></i>
-                    </li>
-                    <li class="nav-item">
-                        <a href="<?php echo base_url() . '/Driver_show/driver_show_ajax' ?>">Driver</a>
-                    </li>
-                </ul>
+            <div class="pl-4 mt-4 page-header mb-0">
+                <h4 class="pl-3 page-title">DRIVER INFORMATION</h4>
             </div>
-            <div class="container-fluid mb-3">
-                <a href="<?php echo base_url() . '/Driver_input/driver_input' ?>" class="btn btn-primary btn-border">
-                    <i class="fas fa-plus"></i>
-                    <b>Add driver</b>
-                </a>
-            </div>
+            <hr width="95%" color="696969">
+            <ul class="pl-2 breadcrumbs">
+                <li class="nav-home">
+                    <a href="<?php echo base_url() . '/Dashboard/dashboard_show'?>">
+                        <i class="flaticon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="flaticon-right-arrow"></i>
+                </li>
+                <li class="nav-item">
+                    <a href="<?php echo base_url() . '/Driver_show/driver_show_ajax'?>">Driver</a>
+                </li>
+            </ul>
+    
+            
+
+
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                        <div class="card-header">
-                            <h4 class="card-title">Driver table</h4>
-                        </div>
                         <div class="card-body">
                             <div class="table-responsive">
-                                <table id="service_list_table" class="display table table-hover">
+                                <table id="driver_list_table" class="display table table-hover cell-border"
+                                    style="border-collapse: collapse !important">
                                     <thead>
-                                        <tr>
+                                        <tr style="background-color: #999999; color: white;">
                                             <th>Id.</th>
                                             <th>Driver name</th>
                                             <th>Identity car</th>
@@ -75,17 +72,7 @@
                                             <th>Action</th>
                                         </tr>
                                     </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Id.</th>
-                                            <th>Driver name</th>
-                                            <th>Identity car</th>
-                                            <th>Car type</th>
-                                            <th>Driver license number</th>
-                                            <th>Tel.</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </tfoot>
+            
                                     <tbody>
                                         <?php for ($i = 0; $i < count($arr_driver); $i++) { ?>
                                             <tr>
@@ -101,9 +88,9 @@
                                                             <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
                                                         </div> -->
 
-                                                        <div class="flex items-center text-sm" onclick="driver_detail(<?php echo $arr_driver[$i]->dri_id ?>)">
-                                                            <?php echo $arr_driver[$i]->dri_name ?>
-                                                        </div>
+                                                    <div class="flex items-center text-sm" onclick="driver_detail(<?php echo $arr_driver[$i]->dri_id ?>)">
+                                                        <?php echo $arr_driver[$i]->dri_name ?>
+                                                    </div>
                                                 </td>
                                                 <!-- name -->
                                                 <td onclick="driver_detail(<?php echo $arr_driver[$i]->dri_id ?>)">
@@ -169,6 +156,34 @@
     </div>
 
     <script>
+        $(document).ready(function() {
+
+            // แทรกปุ่ม เพิ่มลูกค้า
+            var dri_table = $('#driver_list_table').DataTable({
+                "columnDefs": [{
+                    "searchable": false,
+                    "orderable": false,
+                    "targets": [0, 6]
+                }],
+                "order": []
+            });
+
+            //ลำดับ
+            dri_table.on('order.dt search.dt', function() {
+                dri_table.column(0, {
+                    search: 'applied',
+                    order: 'applied'
+                }).nodes().each(function(cell, i) {
+                    cell.innerHTML = i + 1 + '.';
+                });
+            }).draw();
+
+            $("#driver_list_table_filter").append(
+                "<a href='<?php echo base_url() . '/Driver_input/driver_input' ?>' class='btn ml-3' style='background-color: #4B75D8; color: white;'> <i class='fas fa-plus mr-1'></i> ADD </a>"
+            );
+
+        });
+
         function change_location(url) {
             window.location = "https://www.google.com";
         }
