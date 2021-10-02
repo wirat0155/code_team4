2a675956be6fddc750ff728f748392609c3c8909
Pezl05 <pee2013.kittipod@gmail.com>
2021-10-02 23:55:58 +0700

    Set Up Ver 2

--- app/Views/v_set_up.php
+++ app/Views/v_set_up.php
@@ -1,8 +1,11 @@
 <style>
-.fa-truck, .fa-wrench,
-.fa-link, .fa-box {
-    font-size: 500%;
-    margin-top: 35%;
+
+.box-img{
+    position: absolute;
+}
+
+.img-setup {
+    font-size: 50px;
 }
 
 .fa-wrench {
@@ -13,6 +16,7 @@
     filter: FlipH;
     -ms-filter: "FlipH";
 }
+
 </style>
 
 <div class="main-panel">
@@ -42,51 +46,46 @@
                 </li>
             </ul>
 
+
             <div class="row mt-5">
-                <div class="col-md-5 ml-auto">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4 rounded" style="background-color: #1EAFEE; color: white">
-                                <div class="card-round">
-                                    <div class="col-icon">
-                                        <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                            <i class="fas fa-box"></i>
-                                        </div>
-                                    </div>
+                <div class="col-sm-6 col-md-5 ml-auto">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="box-img col-4 icon-big text-center icon-info">
+                                    <i class="img-setup fas fa-box"></i>
                                 </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Container type</h5>
-                                    <hr width="70%" color="696969" align="left">
-                                    <p class="card-text"> Container type set up such as,
-                                        new type, upload image
-                                        and switch on-off</p>
-                                    <p class="card-text text-right"><a href="#" class="text-muted">Set Up ></a></p>
+                                <div class="col-8 col-stats ml-auto">
+                                    <div class="numbers">
+                                        <h4 class="card-title">Container type</h4>
+                                        <hr width="70%" color="696969" align="left">
+                                        <p class="card-category"> Container type set up such as,
+                                            new type, upload image
+                                            and switch on-off </p>
+                                        <p class="card-text text-right mt-3"><a href="#" class="text-muted">Set Up ></a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5 mr-auto">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4 rounded" style="background-color: #1EAFEE; color: white">
-                                <div class="card-round">
-                                    <div class="col-icon">
-                                        <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                            <i class="fas fa-link"></i>
-                                        </div>
-                                    </div>
+                <div class="col-sm-6 col-md-5 mr-auto">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="box-img col-4 icon-big text-center icon-info">
+                                    <i class="img-setup fas fa-link"></i>
                                 </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Container status</h5>
-                                    <hr width="70%" color="696969" align="left">
-                                    <p class="card-text"> Container status set up such as,
-                                        new status, and switch on-off </p>
-                                    <p class="card-text text-right"><a href="#" class="text-muted">Set Up ></a></p>
+                                <div class="col-8 col-stats ml-auto">
+                                    <div class="numbers">
+                                        <h4 class="card-title">Container status</h4>
+                                        <hr width="70%" color="696969" align="left">
+                                        <p class="card-category"> Container status set up such as,
+                                            new status, and switch on-off </p>
+                                        <p class="card-text text-right mt-3"><a href="#" class="text-muted">Set Up ></a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -94,59 +93,52 @@
                 </div>
             </div>
 
-            <div class="row mt-3">
-                <div class="col-md-5 ml-auto">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4 rounded" style="background-color: #1EAFEE; color: white">
-                                <div class="card-round">
-                                    <div class="col-icon">
-                                        <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                            <i class="fas fa-wrench"></i>
-                                        </div>
-                                    </div>
+            <div class="row mt-2">
+                <div class="col-sm-6 col-md-5 ml-auto">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="box-img col-4 icon-big text-center icon-info">
+                                    <i class="img-setup fas fa-wrench"></i>
                                 </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Container size</h5>
-                                    <hr width="70%" color="696969" align="left">
-                                    <p class="card-text"> Container size set up such as,
-                                        new size, width, length, height,
-                                        upload image and switch on-off </p>
-                                    <p class="card-text text-right"><a href="#" class="text-muted">Set Up ></a></p>
+                                <div class="col-8 col-stats ml-auto">
+                                    <div class="numbers">
+                                        <h4 class="card-title">Container size</h4>
+                                        <hr width="70%" color="696969" align="left">
+                                        <p class="card-category"> Container size set up such as,
+                                            new size, width, length, height,
+                                            upload image and switch on-off </p>
+                                        <p class="card-text text-right mt-3"><a href="#" class="text-muted">Set Up ></a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-5 mr-auto">
-                    <div class="card">
-                        <div class="row">
-                            <div class="col-md-4 rounded" style="background-color: #1EAFEE; color: white;">
-                                <div class="card-round ">
-                                    <div class="col-icon">
-                                        <div class="icon-big text-center icon-primary bubble-shadow-small">
-                                            <i class="fas fa-truck"></i>
-                                        </div>
-                                    </div>
+                <div class="col-sm-6 col-md-5 mr-auto">
+                    <div class="card card-stats card-round">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="box-img col-4 icon-big text-center icon-info">
+                                    <i class="img-setup fas fa-truck"></i>
                                 </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Car type</h5>
-                                    <hr width="70%" color="696969" align="left">
-                                    <p class="card-text"> Car type set up such as,
-                                        new type, upload image
-                                        and switch on-off </p>
-                                    <p class="card-text text-right"><a href="#" class="text-muted">Set Up ></a></p>
+                                <div class="col-8 col-stats ml-auto">
+                                    <div class="numbers">
+                                        <h4 class="card-title">Car type</h4>
+                                        <hr width="70%" color="696969" align="left">
+                                        <p class="card-category"> Car type set up such as,
+                                            new type, upload image
+                                            and switch on-off </p>
+                                        <p class="card-text text-right mt-3"><a href="#" class="text-muted">Set Up ></a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
     </div>
 </div>
