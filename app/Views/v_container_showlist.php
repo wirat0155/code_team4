6c89a921e9382509d1132c43d3e5c6d185e2657f
Baiitoey12 <73053663+Baiitoey12@users.noreply.github.com>
2021-10-04 19:56:52 +0700

    Update v_container_showlist.php

--- app/Views/v_container_showlist.php
+++ app/Views/v_container_showlist.php
@@ -130,13 +130,14 @@
                                                     <i class="fas fa-ellipsis-v"></i>
                                                     <div class="menu con_id_<?php echo $arr_container[$i]->con_id ?>"
                                                         style="right: 0;left: auto;">
+                                                        <!-- Button Edit -->
                                                         <div class="item" onclick="change_location('google')">
-                                                            <i class='far fa-money-bill-alt' style="font-size: 110%;">
-                                                            </i> &nbsp;
+                                                            <i class='far fa-edit' style="font-size: 130%;"> </i> &nbsp;
                                                             Edit
                                                         </div>
+                                                        <!-- Button Remove -->
                                                         <div class="item test button"
-                                                            onclick="get_id(<?php echo $arr_container[$i]->con_id ?>)">
+                                                            onclick="get_id(<?php echo $arr_car[$i]->car_id ?>)">
                                                             <i class='fas fa-trash-alt' style="font-size: 130%;"></i>
                                                             &nbsp; &nbsp;
                                                             Remove
