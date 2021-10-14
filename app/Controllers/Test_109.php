05f802bb364a374e7ce51488ab05cfeb012db1f1
Wirat Sakorn <sunneed.2555@gmail.com>
2021-10-14 19:23:55 +0700

    Create Test_109.php

---
+++ app/Controllers/Test_109.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controllers;
+use App\Models\M_cdms_agent;
+
+class Test_109 extends Cdms_controller {
+    public function exam1() {
+        // load model
+        // view
+        // query
+
+        $arr = [1,2,3,4,5];
+
+        // Code ที่นี่
+        echo "ทำถูก";
+        // Code ที่นี่
+    }
+}
