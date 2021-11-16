32a6f63af712983b55cbe3d3d9410b7898aaffdb
62160150 <62160150@go.buu.ac.th>
2021-11-16 20:31:47 +0700

    cancel

--- app/Views/v_service_edit.php
+++ app/Views/v_service_edit.php
@@ -262,7 +262,7 @@
                     </div>
                 </div>
                 <div class="card-action"  id="car_action">
-                    <input type="button" class="ui button" value="Cancle" onclick="window.history.back();">
+                    <input type="button" class="ui button" value="Cancel" onclick="window.history.back();">
                     <button type="submit" class="ui orange button pull-right">
                         Confirm
                     </button>
