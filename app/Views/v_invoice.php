2f9627e4baff1fdb73aa3d38840122669727ae70
Pezl05 <pee2013.kittipod@gmail.com>
2021-12-17 21:15:44 +0700

    v_invoice

--- app/Views/v_invoice.php
+++ app/Views/v_invoice.php
@@ -5,45 +5,11 @@
     <meta http-equiv='Content-Type' content='text/html;charset=utf-8'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
 </head>
 <style>
-    @font-face {
-    font-family: 'THSarabunNew';
-    font-style: normal;
-    font-weight: bold;
-    src: url("{{ asset('fonts/THSarabunNew Bold.ttf')}}") format('truetype');
-}
-@font-face {
-    font-family: 'THSarabunNew';
-    font-style: italic;
-    font-weight: bold;
-    src: url("{{ asset('fonts/THSarabunNew BoldItalic.ttf')}}") format('truetype');
-}
-@font-face {
-    font-family: 'THSarabunNew';
-    font-style: italic;
-    font-weight: normal;
-    src: url("{{ asset('fonts/THSarabunNew Italic.ttf')}}") format('truetype');
-}
-@font-face {
-    font-family: 'THSarabunNew';
-    font-style: normal;
-    font-weight: normal;
-    src: url("{{ asset('fonts/THSarabunNew.ttf')}}") format('truetype');
-}
-    body{
-        font-family: "THSarabunNew";
+    html, body {
+        font-family: "Garuda";
+        font-size: 12pt;
     }
     img {
         size: 150px;
