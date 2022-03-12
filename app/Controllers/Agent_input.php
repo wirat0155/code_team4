7cb5ac6ce47314d0c084610fa5eb2b1097665ff1
Wirat Sakorn <sunneed.2555@gmail.com>
2022-03-12 08:16:42 +0700

    Update Agent_input.php

--- app/Controllers/Agent_input.php
+++ app/Controllers/Agent_input.php
@@ -37,15 +37,6 @@ class Agent_input extends Cdms_controller {
 
     */
     public function agent_insert() {
-        // get post value from agent_input form
-        // $agn_company_name = $this->request->getPost('agn_company_name');
-        // $agn_firstname = $this->request->getPost('agn_firstname');
-        // $agn_lastname = $this->request->getPost('agn_lastname');
-        // $agn_tel = $this->request->getPost('agn_tel');
-        // $agn_address = $this->request->getPost('agn_address');
-        // $agn_tax = $this->request->getPost('agn_tax');
-        // $agn_email = $this->request->getPost('agn_email');
-
         $obj_agn = $this->request->getPost();
 
         // Get agent by agent company name
@@ -58,15 +49,6 @@ class Agent_input extends Cdms_controller {
             // then go to agent input page
             $data = $obj_agn;
             $data['agn_company_name_error'] = "The agent has already used";
-            // $data['agn_company_name'] = $agn_company_name;
-            // $data['agn_firstname'] = $agn_firstname;
-            // $data['agn_lastname'] = $agn_lastname;
-            // $data['agn_tel'] = $agn_tel;
-            // $data['agn_address'] = $agn_address;
-            // $data['agn_tax'] = $agn_tax;
-            // $data['agn_email'] = $agn_email;
-
-            // Send agent data back to agent input
             $this->agent_input($data);
             exit(0);
         }
